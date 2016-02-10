c078959bab264f5ffc4f980e7e5ccd4bc62a2039
jansch <j.schulte@52north.org>
2016-02-10 12:47:50 +0100

    refactor interface service

--- src/js/services/interface.js
+++ src/js/services/interface.js
@@ -207,7 +207,7 @@ angular.module('n52.core.interface', [])
                     angular.extend(series, s);
                     series.parameters.feature = series.parameters.platform;
                     return series;
-                }
+                };
 
                 this.getTsData = function (id, apiUrl, timespan, extendedData) {
                     var requestUrl, isV2 = interfaceUtilsSrvc.isV2(apiUrl);
@@ -270,120 +270,5 @@ angular.module('n52.core.interface', [])
                         delete ts.uom;
                     }
                     return ts;
-                };
-
-                this.getServices = function (apiUrl) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'services', _createRequestConfigs({expanded: true})).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getStations = function (id, apiUrl, params) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'stations/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getPhenomena = function (id, apiUrl, params) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'phenomena/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getCategories = function (id, apiUrl, params) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'categories/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getFeatures = function (id, apiUrl, params) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'features/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getProcedures = function (id, apiUrl, params) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'procedures/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.search = function (apiUrl, arrayParams) {
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'search', _createRequestConfigs({
-                            q: arrayParams.join(',')
-                        })).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getTimeseries = function (id, apiUrl, params) {
-                    if (angular.isUndefined(params))
-                        params = {};
-                    params.expanded = true;
-                    params.force_latest_values = true;
-                    params.status_intervals = true;
-                    params.rendering_hints = true;
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'timeseries/' + _createIdString(id), _createRequestConfigs(params)).then(function (response) {
-                            if (angular.isArray(response.data)) {
-                                var array = [];
-                                angular.forEach(response.data, function (ts) {
-                                    array.push(_pimpTs(ts, apiUrl));
-                                });
-                                resolve(array);
-                            } else {
-                                resolve(_pimpTs(response.data, apiUrl));
-                            }
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
-                };
-
-                this.getTsData = function (id, apiUrl, timespan, extendedData) {
-                    var params = {
-                        timespan: utils.createRequestTimespan(timespan.start, timespan.end),
-                        generalize: statusService.status.generalizeData || false,
-                        expanded: true,
-                        format: 'flot'
-                    };
-                    if (extendedData) {
-                        angular.extend(params, extendedData);
-                    }
-                    return $q(function (resolve, reject) {
-                        $http.get(apiUrl + 'timeseries/' + _createIdString(id) + "/getData", _createRequestConfigs(params)).then(function (response) {
-                            resolve(response.data);
-                        }, function (error) {
-                            _errorCallback(error, reject);
-                        });
-                    });
                 };
             }]);
